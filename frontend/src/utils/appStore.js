2422565d58f1a8fa642d84453c85fcb6304b27ce
Koushik <Koushikbajpayee06@gmail.com>
2026-09-06 00:12:57 +0530

    feat: persist Redux cart state with localStorage

--- frontend/src/utils/appStore.js
+++ frontend/src/utils/appStore.js
@@ -1,9 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice";
+
+const savedCartItems =
+  JSON.parse(localStorage.getItem("cartItems")) || [];
 const appStore = configureStore({
     reducer: {
         cart: cartReducer
-    }
+    },
+     preloadedState: {
+    cart: {
+      items: savedCartItems,
+    },
+  },
 })
+appStore.subscribe(() => {
+  const cartItems =
+    appStore.getState().cart.items;
 
+  localStorage.setItem(
+    "cartItems",
+    JSON.stringify(cartItems)
+  );
+});
 export default appStore
